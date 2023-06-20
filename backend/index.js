4d8e51b3dafd528acac7f76243646cd3df9d83b2
Vishroy Seenarain (RAPP) <vishroy.seenarain@rapp.com>
2023-06-20 12:25:32 +0400

    [APP] - Structure done.

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(8080, () => console.log("App running on port : 8080"));

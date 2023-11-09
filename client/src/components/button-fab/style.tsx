7a3a41efcf2b5116a812c9d30d4188fd65519c9a
Vishroy Seenarain <vishseenarain@gmail.com>
2023-11-10 03:50:52 +0400

    [DEV] [Components] - Btn Fab mobile responsive.

--- client/src/components/button-fab/style.tsx
+++ client/src/components/button-fab/style.tsx
@@ -1,16 +1,9 @@
 import { styled } from 'styled-components';
 
-const StyledWrapper = styled.div<{
-    className?: string
- }>`
-  @media screen and (min-width: 992px) {
-    bottom: 2rem;
-    right: 2rem;
-  }
-
+const StyledWrapper = styled.div`
   .btn-fab {
-    height: 50px;
-    width: 50px;
+    height: 40px;
+    width: 40px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -21,21 +14,26 @@ const StyledWrapper = styled.div<{
     box-shadow: 0px 3px 7px 0px rgba(0, 0, 0, 0.3);
     transition: all 0.3s ease-in-out;
 
+    @media screen and (min-width: 992px) {
+        width: 60px;
+        height: 60px;
+    }
+
     &:hover {
       cursor: pointer;
       background-color: #dcc03e;
     }
-
-    @media screen and (min-width: 992px) {
-      width: 60px;
-      height: 60px;
-    }
   }
+
 
   .btn-fab span {
     color: #323232;
+    font-size: 20px;
     font-weight: 600;
     transition: 0.2s ease-in all;
+    @media screen and (min-width: 992px) {
+        font-size: 28px;
+    }
 
     &.rotate {
         transform: rotate(45deg);

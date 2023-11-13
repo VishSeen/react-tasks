74c2efdcd476e06a393353ef440f6041372c1f85
Vishroy Seenarain <vishseenarain@gmail.com>
2023-11-14 00:40:03 +0400

    [STORYBOOK] - Added router support.

--- client/.storybook/preview.tsx
+++ client/.storybook/preview.tsx
@@ -1,20 +1,21 @@
-import type { Preview } from "@storybook/react";
+import type { Preview, Story } from '@storybook/react';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
-
+import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import '../src/assets/styles/global.scss';
 import { lightTheme, darkTheme, GlobalStyle } from '../src/assets/styles/theme';
+import React from 'react';
 
 const preview: Preview = {
     parameters: {
-        actions: { argTypesRegex: "^on[A-Z].*" },
+        actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
-                date: /Date$/i,
-            },
-        },
-    },
+                date: /Date$/i
+            }
+        }
+    }
 };
 
 export const decorators = [
@@ -25,9 +26,13 @@ export const decorators = [
         },
         defaultTheme: 'light',
         Provider: ThemeProvider,
-        GlobalStyles: GlobalStyle,
-    })
+        GlobalStyles: GlobalStyle
+    }),
+    (Story) => (
+        <MemoryRouter>
+            <Story />
+        </MemoryRouter>
+    )
 ];
-
 
 export default preview;

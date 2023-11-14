298b80af85ea0ab63fd8904c6215315bce15dade
Divesh Heeramun <divesh.heeramun1@gmail.com>
2023-11-14 14:48:33 +0400

    feat: Add missing class names to todo component

--- client/src/components/todo-item/todo-item.tsx
+++ client/src/components/todo-item/todo-item.tsx
@@ -3,26 +3,32 @@ import { TodoItemProps } from '../../types/ComponentProps';
 import * as S from './style';
 
 export const TodoItem: FC<TodoItemProps> = (props) => {
-  const { className, copy, tag, title } = props;
+  const { className = '', copy, tag, title } = props;
+  const baseClassName = 'todo-item';
 
   return (
     <>
-      <S.StyledTodoItem className={className}>
-        <S.StyledTodoItemHeader className="todo-item__wrapper">
+      <S.StyledTodoItem className={`${className} ${baseClassName}`}>
+        <S.StyledTodoItemHeader className={`${baseClassName}__header`}>
           <S.StyledCheckbox
-            className="todo_item__checkbox"
+            className={`${baseClassName}__checkbox`}
             type="checkbox"
             name="tbd"
           />
           <h6>{title}</h6>
           <S.StyledButtonIcon
-            className="todo_item__menu"
+            className={`${baseClassName}__menu`}
             icon="more_vert"
             size="small"
           />
         </S.StyledTodoItemHeader>
-        {copy && <S.StyledTodoCopy>{copy}</S.StyledTodoCopy>}
-        {tag && <S.StyledChipTag tag={tag} />}
+        {copy && <S.StyledTodoCopy className={`${baseClassName}__copy`}>{copy}</S.StyledTodoCopy>}
+        {tag && (
+          <S.StyledChipTag
+            className={`${baseClassName}__tag`}
+            tag={tag}
+          />
+        )}
       </S.StyledTodoItem>
     </>
   );

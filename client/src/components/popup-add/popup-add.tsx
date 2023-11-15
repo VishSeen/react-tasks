9f8a94374de83bad3e16799f7c28043d029087b8
Vishroy Seenarain <vishseenarain@gmail.com>
2023-11-15 23:00:55 +0400

    [DEV] - Added Popup Add forn adding notes, todos etc.

---
+++ client/src/components/popup-add/popup-add.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from "react";
+import StyledPopupAdd from "./style";
+import { PopupAddProps } from "../../types/ComponentProps";
+
+
+
+const PopupNew: FunctionComponent<PopupAddProps> = ({
+    className,
+    formItems
+}) => {
+    return (
+        <StyledPopupAdd>
+
+        </StyledPopupAdd>
+    )
+}
+
+export default PopupNew;
---
+++ client/src/components/popup-add/style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledPopupAdd = styled.div`
+
+`;
+
+export default StyledPopupAdd;

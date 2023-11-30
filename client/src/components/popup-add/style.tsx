b66433ad2d1ad9e6198351667e0f6e11c3640f1f
Vishroy Seenarain <vishseenarain@gmail.com>
2023-12-01 01:31:42 +0400

    [DEV] - Added modal todo.

--- client/src/components/popup-add/style.tsx
+++ client/src/components/popup-add/style.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 const StyledPopupAdd = styled.div`
-
+    width: 287px;
+    height: 483px;
+    padding: 22px 16px;
+    border-radius: 16px;
+    background-color: ${props => props.theme.background.navBar};
+    display: flex;
+    flex-direction: column;
 `;
 
 export default StyledPopupAdd;
